6dc3114d2d5487ea59eb1a6c25b006952d5600c9
Ramon Peek <ramonpeek@hotmail.com>
2020-06-21 23:16:01 +0200

    Added link to twitch

--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import TwitchSettings from '../twitch_settings.json';
 
+/* INITIALIZE VUE */
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
+
+/* INITIALIZE TWITCH BOT */
+const tmi = require('tmi.js');
+
+const options = {
+    identity: {
+        username: TwitchSettings.bot_username,
+        password: TwitchSettings.bot_oauth
+    },
+    channels: TwitchSettings.bot_channels
+};
+
+const client = new tmi.client(options);
+
+client.on("message", (/*target, context, msg, self*/) => {
+    console.log(`* Executed command`);
+});
+
+client.on("connected", (addr, port) => {
+    console.log(`* Connected to ${addr}:${port}`);
+});
+
+client.connect();
