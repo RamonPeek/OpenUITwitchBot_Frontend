6daa2bb7e45daedcbdf825f94ac212ada1d829ff
Ramon Peek <ramonpeek@hotmail.com>
2020-06-22 00:00:23 +0200

    Fixed routing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
+    <router-link to="/dashboard">Dashboard</router-link>
     <router-view></router-view>
   </v-app>
 </template>
